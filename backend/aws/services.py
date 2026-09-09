291669f1efcdffa54f7e423e49b138b1d5150fbf
relkarsanket-oss <relkarsanket@gmail.com>
2026-09-09 16:31:55 +0530

    Add EC2 instance retrieval

--- backend/aws/services.py
+++ backend/aws/services.py
@@ -5,3 +5,32 @@ from backend.aws.config import AWS_REGION
 
 def get_sts_client():
     return boto3.client("sts", region_name=AWS_REGION)
+
+
+def get_ec2_client():
+    return boto3.client("ec2", region_name=AWS_REGION)
+
+
+def get_ec2_instances():
+    ec2 = get_ec2_client()
+
+    response = ec2.describe_instances()
+
+    instances = []
+
+    for reservation in response.get("Reservations", []):
+        for instance in reservation.get("Instances", []):
+            instances.append(
+                {
+                    "instance_id": instance.get("InstanceId"),
+                    "instance_type": instance.get("InstanceType"),
+                    "state": instance.get("State", {}).get("Name"),
+                    "private_ip": instance.get("PrivateIpAddress"),
+                    "public_ip": instance.get("PublicIpAddress"),
+                    "availability_zone": instance.get("Placement", {}).get(
+                        "AvailabilityZone"
+                    ),
+                }
+            )
+
+    return instances
